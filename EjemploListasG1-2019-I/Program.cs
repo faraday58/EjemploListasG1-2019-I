24fff9db4e53eb725ab217fdee7d9b92e06b5c1f
faraday58 <jarodriguezvera@gmail.com>
2018-10-03 16:33:04 -0500

    Ejemplo del manejo de listas

--- EjemploListasG1-2019-I/Program.cs
+++ EjemploListasG1-2019-I/Program.cs
@@ -1,15 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using ContenidoMusical;
 namespace EjemploListasG1_2019_I
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            
+
+            
+
+            List<Musica > milistamusical = new List<Musica>();
+
+            Musica mimusica = new Musica("Mátalas","Ranchera","Compilación Regional","Alejandro Fernandez",180,2003);
+            Musica cancion2 = new Musica("Lo que no fue no será", "Balada", "Sinfónico", "El principe de la canción", 225, 2018);
+            Musica cancion3 = new Musica("El sinaloense", "Banda", "Grandes hits", "Banda El recodo", 134, 2000);
+
+
+            milistamusical.Add(mimusica);
+            milistamusical.Add(cancion2);
+            milistamusical.Add(cancion3);
+
+            Console.WriteLine("Album " + milistamusical[0].Album);
+
+            Console.WriteLine("¿Qué canción te gustaría escuchar? :D");
+            string aux= Console.ReadLine();
+
+            for(int i=0; i<milistamusical.Count; i++)
+            {
+                if (milistamusical[i].Nomcancion==aux)
+                {
+                    Console.WriteLine("Reproduciendo " + milistamusical[i].Nomcancion + " del álbum " + milistamusical[i].Album + " del artista " + milistamusical[i].Artista + " con duración de " + milistamusical[i].Duracion + " segundos " + " del año " + milistamusical[i].Año);
+
+                }
+
+            }
+
+
+            Console.WriteLine("¿Qué canción te gustaría eliminar ?");
+            aux = Console.ReadLine();
+
+            for (int i = 0; i < milistamusical.Count; i++)
+            {
+                if (milistamusical[i].Nomcancion == aux)
+                {
+                    milistamusical.RemoveAt(i);
+                }
+
+            }
+
+            for (int i = 0; i < milistamusical.Count; i++)
+            {
+                Console.WriteLine("Nombre" + milistamusical[i].Nomcancion + " del álbum " + milistamusical[i].Album + " del artista " + milistamusical[i].Artista + " con duración de " + milistamusical[i].Duracion + " segundos " + " del año " + milistamusical[i].Año);
+            }
+
+
+
+
+
+
         }
     }
 }
---
+++ Musica/MusicaClass.cs
@@ -0,0 +1,38 @@
+﻿
+
+namespace ContenidoMusical
+{
+    public class Musica
+    {
+        #region Atributos
+        private string nomcancion;
+        private string genero;
+        private string album;
+        private string artista;
+        private ushort duracion;
+        private ushort año;
+        #endregion
+
+        #region Propiedades
+        public string Nomcancion { get => nomcancion; set => nomcancion = value; }
+        public string Genero { get => genero; set => genero = value; }
+        public string Album { get => album; set => album = value; }
+        public string Artista { get => artista; set => artista = value; }
+        public ushort Duracion { get => duracion; set => duracion = value; }
+        public ushort Año { get => año; set => año = value; }
+        #endregion
+
+        #region Constructor
+        public Musica(string nomcancion, string genero, string album, string artista, ushort duracion, ushort año)
+        {
+            Nomcancion = nomcancion;
+            Genero = genero;
+            Album = album;
+            Artista = artista;
+            Duracion = duracion;
+            Año = año;
+        }
+        #endregion
+
+    }
+}
